3ec122881bddf076367746dccd06b5fdf95ecec0
Abishli-Kamal <tu7ldxfzy@code.edu.az>
2022-04-04 00:24:04 +0400

    Task hazirdi

---
+++ Task_Delegate/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Task_Delegate
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] Arr = { 10, 20, 30, 40, 10, 50 };
+            Action<double ,double> product = delegate (double x,double y)
+              {
+                  double result = 0;
+                  int i = 0;
+                  for (i=0; i < Arr.Length; i++)
+                  {
+                      result+= Arr[i];
+                  }
+                  Console.WriteLine($"Pruduct count:{i}");
+                  Console.WriteLine($"Prudct TotalPrice:{ result}");
+              };
+            product(1,1);
+        }
+
+    }
+}
